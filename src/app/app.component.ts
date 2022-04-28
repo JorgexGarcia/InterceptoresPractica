3a91817eac50394a66249de656b69104a6a515d8
Jorge García González <jorgegarcia.java@gmail.com>
2022-04-28 09:18:34 +0200

    Interceptores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ServiceService} from "./services/service.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'interceptores';
+
+  constructor(private service: ServiceService) {
+
+    this.service.obtenerUsuarios().subscribe({
+      next: (resp => {
+        console.log(resp)
+      }),
+      error: (_ => {
+        console.warn('error')
+      })
+    });
+  }
+
+
+
 }
